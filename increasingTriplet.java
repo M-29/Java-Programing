b0455d1f3c8976d50c8bcdf825fccfbecf64c353
Mannat Sapra <63151423+M-29@users.noreply.github.com>
2026-01-11 09:39:46 +0530

    Create increasingTriplet.java

---
+++ increasingTriplet.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int left = Integer.MAX_VALUE;
+        int mid = Integer.MAX_VALUE;
+        for(int n : nums){
+            if(n <= left){
+                left = n;
+            }else if(n <= mid){
+                mid = n;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
