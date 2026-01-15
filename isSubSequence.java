f27d6074b9171d3283ef389b8b11c0ba0a2a1b59
Mannat Sapra <63151423+M-29@users.noreply.github.com>
2026-01-15 20:46:53 +0530

    Create isSubSequence.java

---
+++ isSubSequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int index = 0;
+        if(s == null || s.equals("")){
+            return true;
+        }
+        for(int i =0; i<t.length(); i++){
+            if(s.charAt(index) == t.charAt(i)){
+                index++;
+            }
+            if(index == s.length())
+            {
+                return true;
+            }
+        }
+            return false;
+    }
+}
