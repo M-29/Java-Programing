6e4d54d5debf896d07d025a6ce3170274005c61f
Mannat Sapra <63151423+M-29@users.noreply.github.com>
2026-04-05 08:19:18 +0530

    Create MergSortByCreatingCopy.java

---
+++ Sorting/MergSortByCreatingCopy.java
@@ -0,0 +1,46 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Arrays;
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        int[] arr = {5,3,2,4,1,19,28};
+        System.out.println(Arrays.toString(mergeSort(arr)));
+    }
+    static int[] mergeSort(int[] arr){
+        if(arr.length == 1){
+            return arr;
+        }
+        int mid  = arr.length/2;
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+        return mergeArray(left,right);
+    }
+    static int[] mergeArray(int[] first, int[] second){
+        int[] mergedArray = new int[first.length + second.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while(i < first.length && j < second.length){
+            if(first[i] < second[j]){
+                mergedArray[k] = first[i];
+                i++;
+            }else{
+                mergedArray[k] = second[j];
+                j++;
+            }
+            k++;
+        }
+        while(i < first.length){
+            mergedArray[k] = first[i];
+                i++;
+                k++;
+        }
+        while(j < second.length){
+            mergedArray[k] = second[j];
+                j++;
+                k++;
+        }
+        return mergedArray;
+    }
+}
