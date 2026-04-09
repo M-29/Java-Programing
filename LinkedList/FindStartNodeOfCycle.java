ce590cdebfd62f2f735efb2609089d19de7c15d8
Mannat Sapra <63151423+M-29@users.noreply.github.com>
2026-04-09 08:32:16 +0530

    Create FindStartNodeOfCycle.java

---
+++ LinkedList/FindStartNodeOfCycle.java
@@ -0,0 +1,58 @@
+// Approach 1
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode f = head;
+        ListNode s = head;
+        
+        int len = LengthOfCycle(s);
+        
+        if(len == 0){
+            return null;
+        }
+        while(len > 0){
+            s = s.next;
+            len--;
+        }
+        while(f != s){
+            f = f.next;
+            s = s.next;
+        }
+        return s;
+    }
+    public int LengthOfCycle(ListNode node){
+        ListNode fast = node;
+        ListNode slow = node;
+        int length = 0;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                ListNode temp = slow;
+                do{
+                    temp = temp.next;
+                    length++;
+                }while(temp != slow);
+                break;
+            }
+        }
+        return length;
+    }
+}
+//Approach 2
+ public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode start = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow){
+                while(start != slow){
+                    start = start.next;
+                    slow = slow.next;
+                }
+                return start;
+            }
+        }
+        return null;
+    }
